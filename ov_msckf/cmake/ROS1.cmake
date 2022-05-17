56af4f5f53abb81ab2a9e2d80d47828c2db0a09a
matt.turi <matt.turi@modalai.com>
2022-05-17 14:09:25 -0700

    cmake cleanup for thirdparty stuff

--- ov_msckf/cmake/ROS1.cmake
+++ ov_msckf/cmake/ROS1.cmake
@@ -39,34 +39,6 @@ list(APPEND thirdparty_libraries
         ${catkin_LIBRARIES}
 )
 
-# If we are not building with ROS then we need to manually link to its headers
-# This isn't that elegant of a way, but this at least allows for building without ROS
-# If we had a root cmake we could do this: https://stackoverflow.com/a/11217008/7718197
-# But since we don't we need to basically build all the cpp / h files explicitly :(
-if (NOT catkin_FOUND OR NOT ENABLE_ROS)
-
-    message(STATUS "MANUALLY LINKING TO OV_CORE LIBRARY....")
-    include_directories(${CMAKE_SOURCE_DIR}/../ov_core/src/)
-    file(GLOB_RECURSE OVCORE_LIBRARY_SOURCES "${CMAKE_SOURCE_DIR}/../ov_core/src/*.cpp")
-    list(FILTER OVCORE_LIBRARY_SOURCES EXCLUDE REGEX ".*test_webcam\\.cpp$")
-    list(FILTER OVCORE_LIBRARY_SOURCES EXCLUDE REGEX ".*test_tracking\\.cpp$")
-    list(APPEND LIBRARY_SOURCES ${OVCORE_LIBRARY_SOURCES})
-    file(GLOB_RECURSE OVCORE_LIBRARY_HEADERS "${CMAKE_SOURCE_DIR}/../ov_core/src/*.h")
-    list(APPEND LIBRARY_HEADERS ${OVCORE_LIBRARY_HEADERS})
-
-    message(STATUS "MANUALLY LINKING TO OV_INIT LIBRARY....")
-    include_directories(${CMAKE_SOURCE_DIR}/../ov_init/src/)
-    file(GLOB_RECURSE OVINIT_LIBRARY_SOURCES "${CMAKE_SOURCE_DIR}/../ov_init/src/*.cpp")
-    list(FILTER OVINIT_LIBRARY_SOURCES EXCLUDE REGEX ".*test_dynamic_init\\.cpp$")
-    list(FILTER OVINIT_LIBRARY_SOURCES EXCLUDE REGEX ".*test_dynamic_mle\\.cpp$")
-    list(FILTER OVINIT_LIBRARY_SOURCES EXCLUDE REGEX ".*test_simulation\\.cpp$")
-    list(FILTER OVINIT_LIBRARY_SOURCES EXCLUDE REGEX ".*Simulator\\.cpp$")
-    list(APPEND LIBRARY_SOURCES ${OVINIT_LIBRARY_SOURCES})
-    file(GLOB_RECURSE OVINIT_LIBRARY_HEADERS "${CMAKE_SOURCE_DIR}/../ov_init/src/*.h")
-    list(FILTER OVINIT_LIBRARY_HEADERS EXCLUDE REGEX ".*Simulator\\.h$")
-    list(APPEND LIBRARY_HEADERS ${OVINIT_LIBRARY_HEADERS})
-
-endif ()
 
 ##################################################
 # Make the shared library
@@ -89,6 +61,17 @@ if (catkin_FOUND AND ENABLE_ROS)
 endif ()
 file(GLOB_RECURSE LIBRARY_HEADERS "src/*.h")
 add_library(ov_msckf_lib SHARED ${LIBRARY_SOURCES} ${LIBRARY_HEADERS})
+
+if (NOT catkin_FOUND OR NOT ENABLE_ROS)
+
+    message(STATUS "MANUALLY LINKING TO OV_CORE LIBRARY....")
+    include_directories(${CMAKE_SOURCE_DIR}/../ov_core/src/)
+    target_link_libraries(ov_msckf_lib ov_core_lib)
+    include_directories(${CMAKE_SOURCE_DIR}/../ov_init/src/)
+    target_link_libraries(ov_msckf_lib ov_init_lib)
+
+endif ()
+
 target_link_libraries(ov_msckf_lib ${thirdparty_libraries})
 target_include_directories(ov_msckf_lib PUBLIC src/)
 install(TARGETS ov_msckf_lib
