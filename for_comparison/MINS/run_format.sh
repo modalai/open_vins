834a9cc01f6611904c39a983c4f58d3911ee4632
zauberflote <joao.cotta@modalai.com>
2026-03-30 14:32:16 -0700

    delete mins ref

--- for_comparison/MINS/run_format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# sudo apt install clang-format
-find mins/ -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
-
-# sudo apt install libc++-dev clang-tidy
-#find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-tidy {} \;
-
